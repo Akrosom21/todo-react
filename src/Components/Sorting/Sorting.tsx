9cfa75c93de3000b42d05cf520b5207a2ab1a1fb
Max Rebedailo <akrosom21@gmail.com>
2020-12-17 19:44:54 +0200

    add sorting of tasks

---
+++ src/Components/Sorting/Sorting.tsx
@@ -0,0 +1,22 @@
+import React, {FC} from "react";
+import styles from './Sorting.module.css'
+import {taskType} from "../../Store/inputReducer";
+
+type propsType = {
+    tasksCount: number
+    onAllTasks: any
+    onIncompletedTasks: any
+    onCompletedTasks: any
+}
+export const Sorting: FC<propsType> = (props) => {
+    return (
+        <div className={styles.sorting}>
+            <div className="sorting__count">{props.tasksCount}items left</div>
+            <div className="sorting__btns">
+                <button onClick={props.onAllTasks} className="sorting__all">All</button>
+                <button onClick={props.onIncompletedTasks} className="sorting__active">Active</button>
+                <button onClick={props.onCompletedTasks} className="sorting__completed">Completed</button>
+            </div>
+        </div>
+    )
+}
--- src/Components/TodoField/TodoField.tsx
+++ src/Components/TodoField/TodoField.tsx
@@ -1,32 +1,40 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {TodoInput} from "../TodoInput/TodoInput";
 import {appStoreType} from "../../Store/store";
 import {useSelector} from 'react-redux'
 import {Task} from "../Task/Task";
 import {taskType} from "../../Store/inputReducer";
 import styles from './TodoField.module.css'
+import {Sorting} from "../Sorting/Sorting";
 
 export const TodoField = () => {
+    //setting tasks array according to requirements (all, completed, incompleted)
     const tasksArr = useSelector((state: appStoreType) => state.todoInput.task)
-    const completedTasks = (tasksArr) => {
-        return tasksArr.filter((item: taskType) => {
-            return item.completed
-        })
+    const allTasks = tasksArr.map((item: taskType) => <Task key={item.id} id={item.id}
+                                                            completed={item.completed}
+                                                            taskText={item.text}/>)
+    const completedTasks = allTasks.filter((item) => item.props.completed)
+    const incompletedTasks = allTasks.filter((item) => !item.props.completed)
+    const [tasks, setTasks] = useState<Array<taskType>>(allTasks)
+    const onCompletedTasks = (): void => {
+        setTasks(completedTasks)
     }
-    const tasksCompletedArr = completedTasks(tasksArr)
-    const incompletedTasks = (tasksArr) => {
-        return tasksArr.filter((item: taskType) => {
-            return !item.completed
-        })
+    const onIncompletedTasks = (): void => {
+        setTasks(incompletedTasks)
     }
-    const tasksIncompletedArr = incompletedTasks(tasksArr)
-    const tasks = tasksArr.map((item: taskType) => <Task key={item.id} id={item.id}
-                                                         completed={item.completed}
-                                                         taskText={item.text}/>)
+    const onAllTasks = (): void => {
+        setTasks(allTasks)
+    }
+    useEffect(() => {
+        setTasks(allTasks)
+    }, [tasksArr])
+    const tasksCount = incompletedTasks.length
     return (
         <div className={styles.todoField}>
             <TodoInput/>
             {tasks}
+            <Sorting onAllTasks={onAllTasks} onIncompletedTasks={onIncompletedTasks}
+                     onCompletedTasks={onCompletedTasks} tasksCount={tasksCount}/>
         </div>
     )
 }
